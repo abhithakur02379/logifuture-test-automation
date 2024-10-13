30a2d4162111827bdb6df4b4c6fd173aba69fe88
abhisheksingh02379 <abhishek.singh.ext@tlscontact.com>
2024-10-13 15:25:08 +0100

    GoogleTest

--- cypress/e2e/GoogleTest.cy.js
+++ cypress/e2e/GoogleTest.cy.js
@@ -1,12 +1,10 @@
 describe('Google Page Title Test', () => {
     it('should check the title of the page', () => {
-      // Determine environment from Cypress environment variables
+
       const url = Cypress.env('url') || Cypress.config('baseUrl');
-      
-      // Visit the specified URL
+
       cy.visit(url);
-      
-      // Assert that the title contains "Google"
+
       cy.title().should('include', 'Google');
     });
   });
