e5a1d5dee8e55805cb6831e84f14fed66788331a
abhisheksingh02379 <abhishek.singh.ext@tlscontact.com>
2024-10-13 15:54:03 +0100

    adding packages

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,32 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+const path = require("path");
+const fs = require("fs-extra");
+
+function getConfigurationByFilename(env) {
+  const pathToConfigFile = path.resolve(".", "cypress/fixtures", `${env}.json`);
+
+  return fs.readJson(pathToConfigFile);
+}
+module.exports = defineConfig({
+  // projectId: 'rhyoxi',
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 60000,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  chromeWebSecurity: false,
+  trashAssetsBeforeRuns: true,
+
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const file = config.env.configFile || "dev";
+      return getConfigurationByFilename(file);
+    },
+    env: {
+      environment: "dev",
     },
   },
-};
+
+  
+});
